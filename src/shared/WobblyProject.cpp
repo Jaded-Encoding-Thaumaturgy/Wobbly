Jaded-Encoding-Thaumaturgy/Wobbly

commit a9272a23d81385f0ff373cdc6b6b7a141888eefe

Set `rff=True` when using BestSource (#33)
* Set `rff=True` when using BestSource

Co-authored-by: sgt0 <140186177+sgt0@users.noreply.github.com>

* Run build on PRs

---------

Co-authored-by: Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>

src/shared/WobblyProject.cpp
@@ -4128,8 +4128,20 @@ void WobblyProject::presetsToScript(std::string &script) const {
 }
 
 
+const char *WobblyProject::getArgsForSourceFilter() const {
+    if (source_filter == "bs.VideoSource")
+        return ", rff=True";
+    return "";
+}
+
+
 void WobblyProject::sourceToScript(std::string &script, bool save_node) const {
-    std::string src = "src = c." + source_filter + "(r'" + handleSingleQuotes(input_file) + "')\n";
+    std::string src = std::format(
+        "src = c.{}(r'{}'{})\n",
+        source_filter,
+        handleSingleQuotes(input_file),
+        getArgsForSourceFilter()
+    );
 
     if (save_node) {
     script +=

src/wibbly/WibblyJob.cpp
@@ -235,24 +235,34 @@ void WibblyJob::headerToScript(std::string &script) const {
             "\n";
 }
 
+const char *WibblyJob::getArgsForSourceFilter() const {
+    if (source_filter == "bs.VideoSource")
+        return ", rff=True";
+    return "";
+}
+
 
 void WibblyJob::sourceToScript(std::string &script) const {
     std::string fixed_input_file = handleSingleQuotes(input_file);
 
-    script +=
-            "if wibbly_last_input_file == r'" + fixed_input_file + "':\n"
+    script += std::format(
+            "if wibbly_last_input_file == r'{0}':\n"
             "    try:\n"
             "        src = vs.get_output(index=1)\n"
             "        if isinstance(src, vs.VideoOutputTuple):\n"
             "            src = src[0]\n"
             "    except KeyError:\n"
-            "        src = c." + source_filter + "(r'" + fixed_input_file + "')\n"
+            "        src = c.{1}(r'{0}'{2})\n"
             "        src.set_output(index=1)\n"
             "else:\n"
-            "    src = c." + source_filter + "(r'" + fixed_input_file + "')\n"
+            "    src = c.{1}(r'{0}'{2})\n"
             "    src.set_output(index=1)\n"
-            "    wibbly_last_input_file = r'" + fixed_input_file + "'\n"
-            "\n";
+            "    wibbly_last_input_file = r'{0}'\n"
+            "\n",
+            fixed_input_file,
+            source_filter,
+            getArgsForSourceFilter()
+    );
 }
 
 

.github/workflows/windows.yml
@@ -10,8 +10,9 @@ on:
       - 'Makefile.am'
       - 'qt.conf'
       - '.github/workflows/windows.yml'
+  pull_request:
   workflow_dispatch:
-    
+
 
 jobs:
   build-windows:
@@ -23,7 +24,7 @@ jobs:
 
     steps:
     - name: Checkout repo
-      uses: actions/checkout@v3
+      uses: actions/checkout@v4
       with:
         fetch-depth: 1
 
@@ -57,7 +58,7 @@ jobs:
         cp -r $MSYSTEM_PREFIX/share/qt5/plugins/{platforms,styles} deploy
 
     - name: Upload
-      uses: actions/upload-artifact@v3
+      uses: actions/upload-artifact@v4
       with:
         name: Wobbly-win64
         path: deploy

src/shared/WobblyProject.h
@@ -438,6 +438,7 @@ class WobblyProject : public QObject {
         void customListsToScript(std::string &script, PositionInFilterChain position) const;
         void headerToScript(std::string &script) const;
         void presetsToScript(std::string &script) const;
+        const char *getArgsForSourceFilter() const;
         void sourceToScript(std::string &script, bool save_node) const;
         void trimToScript(std::string &script) const;
         void fieldHintToScript(std::string &script) const;

src/wibbly/WibblyJob.h
@@ -67,6 +67,7 @@ class WibblyJob {
 
     double fades_threshold;
 
+    const char *getArgsForSourceFilter() const;
 
     void headerToScript(std::string &script) const;
     void sourceToScript(std::string &script) const;

src/wobbly/WobblyWindow.cpp
@@ -3878,6 +3878,11 @@ void WobblyWindow::openProject() {
     }
 }
 
+const char *WobblyWindow::getArgsForSourceFilter(const QString &source_filter) {
+    if (source_filter == "bs.VideoSource")
+        return ", rff=True";
+    return "";
+}
 
 void WobblyWindow::realOpenVideo(const QString &path) {
     try {
@@ -3897,8 +3902,11 @@ void WobblyWindow::realOpenVideo(const QString &path) {
                     "\n"
                     "c = vs.core\n"
                     "\n"
-                    "c.%1(r'%2').set_output()\n");
-        script = script.arg(source_filter).arg(QString::fromStdString(handleSingleQuotes(path.toStdString())));
+                    "c.%1(r'%2'%3).set_output()\n");
+        script = script
+            .arg(source_filter)
+            .arg(QString::fromStdString(handleSingleQuotes(path.toStdString())))
+            .arg(getArgsForSourceFilter(source_filter));
 
         QApplication::setOverrideCursor(Qt::WaitCursor);
 

src/wobbly/WobblyWindow.h
@@ -320,6 +320,8 @@ private:
     void keyPressEvent(QKeyEvent *event);
     void wheelEvent(QWheelEvent *event);
 
+    const char *getArgsForSourceFilter(const QString &source_filter);
+
     void realOpenProject(const QString &path);
     void realOpenVideo(const QString &path);
     void realSaveProject(const QString &path);

commit f8b84955a6ba78773bf425acbfd086f62dcb4112

Fix compilation
std::format was introduced in C++20 and there is no reason to bump
the required compiler for the sake of these two lines of code.
Build was broken since a9272a23d81385f0ff373cdc6b6b7a141888eefe

src/shared/WobblyProject.cpp
@@ -4132,12 +4132,7 @@ const char *WobblyProject::getArgsForSourceFilter() const {
 
 
 void WobblyProject::sourceToScript(std::string &script, bool save_node) const {
-    std::string src = std::format(
-        "src = c.{}(r'{}'{})\n",
-        source_filter,
-        handleSingleQuotes(input_file),
-        getArgsForSourceFilter()
-    );
+    std::string src = "src = c." + source_filter + "(r'" + handleSingleQuotes(input_file) + "'" + getArgsForSourceFilter() + ")\n";
 
     if (save_node) {
     script +=

src/wibbly/WibblyJob.cpp
@@ -245,24 +245,20 @@ const char *WibblyJob::getArgsForSourceFilter() const {
 void WibblyJob::sourceToScript(std::string &script) const {
     std::string fixed_input_file = handleSingleQuotes(input_file);
 
-    script += std::format(
-            "if wibbly_last_input_file == r'{0}':\n"
+    script +=
+            "if wibbly_last_input_file == r'" + fixed_input_file + "':\n"
             "    try:\n"
             "        src = vs.get_output(index=1)\n"
             "        if isinstance(src, vs.VideoOutputTuple):\n"
             "            src = src[0]\n"
             "    except KeyError:\n"
-            "        src = c.{1}(r'{0}'{2})\n"
+            "        src = c." + source_filter + "(r'" + fixed_input_file + "'" + getArgsForSourceFilter() + ")\n"
             "        src.set_output(index=1)\n"
             "else:\n"
-            "    src = c.{1}(r'{0}'{2})\n"
+            "    src = c." + source_filter + "(r'" + fixed_input_file + "'" + getArgsForSourceFilter() + ")\n"
             "    src.set_output(index=1)\n"
-            "    wibbly_last_input_file = r'{0}'\n"
-            "\n",
-            fixed_input_file,
-            source_filter,
-            getArgsForSourceFilter()
-    );
+            "    wibbly_last_input_file = r'" + fixed_input_file + "'\n"
+            "\n";
 }
 
 